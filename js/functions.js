8f99e8054e15d690deb6e6c81f3561777b8cc07a
Rovaniemi <mauri.karlin@helsinki.fi>
2016-11-30 03:19:56 +0200

    add data.json for testing

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,25 @@
-function asetaArvo() {
-	document.getElementById("runous").innerHTML = 'asodifj';
+function luoRuno() {
+    //Aloitussana
+
+    var sana = document.getElementById("runonAloitusSana").value;
+    //Sanojen määrä
+    var maara = document.getElementById("sanojenMaara").value;
+    var sananSailytys = '';
+    var runo = '';
+
+    var j = 1;
+    //data.jsonissa määritelty var data
+    for(var i = 0; i < maara; i++){
+        if(data.hasOwnProperty(sana)){
+            runo += sana + ' ';
+            sananSailytys = sana;
+            sana = data[sana][j];
+            j = 1;
+        } else {
+            sana = data[sananSailytys][j++];
+        }
+    }
+
+
+    document.getElementById("runous").innerHTML = runo;
 }
